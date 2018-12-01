e39e7ffef400404fa93805cde4aefa8006820257
Ginny Mangat <mangatgrinpal@gmail.com>
2018-11-30 16:08:22 -0800

    navbar updated for mobile

--- app/javascript/components/Nav.js
+++ app/javascript/components/Nav.js
@@ -25,17 +25,23 @@ class Nav extends React.Component {
 
 
 		return (
-			<nav className="navbar navbar-expand-lg navbar-light bg-light">
-			  <a className="navbar-brand" href="/">Snail Mail Now</a>
-			  <ul className="navbar-nav ml-auto">
-			  	<li className={homeButtonStyle}>
-			  		<a className="nav-link" href="/">Home</a>
-						
-					</li>
-					<li className={aboutButtonStyle}>
-						<a className="nav-link" href="/about">About</a>
-					</li>
-			  </ul>
+			<nav className="navbar navbar-expand-lg navbar-toggler navbar-light bg-light">
+				<a className="navbar-brand" href="/">Snail Mail Now</a>
+				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+			    <span className="navbar-toggler-icon"></span>
+			  </button>
+				
+				<div className="collapse navbar-collapse" id="navbarToggler">
+				  <ul className="navbar-nav ml-auto">
+				  	<li className={homeButtonStyle}>
+				  		<a className="nav-link" href="/">Home</a>
+							
+						</li>
+						<li className={aboutButtonStyle}>
+							<a className="nav-link" href="/about">About</a>
+						</li>
+				  </ul>
+				</div>
 			</nav>
 		)
 	}
